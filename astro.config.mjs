4026c2c964ba0c2069f0f76aee99e692440ece66
xiaokhkh <xiaokhkh@users.noreply.github.com>
2026-05-15 15:33:58 +0800

    chore: set production site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://konh-blog.vercel.app',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
