4c82549dda86a2e71dc446098d1b0fe80b0240da
xiaokhkh <xiaokhkh@users.noreply.github.com>
2026-05-16 22:14:47 +0800

    feat: add seo and geo metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://konh-blog.vercel.app',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark-default',
